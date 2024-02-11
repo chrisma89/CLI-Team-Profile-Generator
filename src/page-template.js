f9a029420f6f53fc6a79b08dd1751a434e3acf8c
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-11 22:57:33 +0000

    css added for the individual cards and colors added for header and text

--- src/page-template.js
+++ src/page-template.js
@@ -4,7 +4,7 @@ const generateTeam = team => {
     // creates the manager html
     const generateManager = manager => {
         return `
-        <div class="card employee-card">
+        <div class="card col-md-3 employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
@@ -23,7 +23,7 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
+        <div class="card col-md-3 employee-card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
@@ -42,7 +42,7 @@ const generateTeam = team => {
     // creates the html for interns
     const generateIntern = intern => {
         return `
-        <div class="card employee-card">
+        <div class="card col-md-3 employee-card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
@@ -55,6 +55,7 @@ const generateTeam = team => {
         </ul>
     </div>
 </div>
+
         `;
     };
 
@@ -75,7 +76,9 @@ const generateTeam = team => {
         .join("")
     );
 
-    return html.join("");
+    const htmlCards= html.join("");
+    
+    return `<div class="row justify-content-center">${htmlCards}</div>`;
 
 }
 
@@ -99,13 +102,13 @@ module.exports = team => {
 <body>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-12 mb-3 mt-4 p-5 bg-primary text-white rounded team-heading">
+            <div class="col-12 mb-3 mt-4 p-5 rounded team-heading">
                 <h1 class="text-center">My Team</h1>
             </div>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
+    <div class="container-fluid">
+        <div class="row col-12">
             <div class="team-area col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
             </div>
