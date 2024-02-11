51dfc0a3665b649881aa34c6a46b9aa86d4ad50c
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-11 23:10:14 +0000

    comments added to index.js

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Code to gather information about the development team members, and render the HTML file.
 
+// initiate array to store individual member's information
 let team = [];
 
-// Questions to prompt user for Manager's details.
+// function to prompt user to get Manager's details.
 function managerInfo() {
   return inquirer
     .prompt([
@@ -43,17 +44,25 @@ function managerInfo() {
       },
     ])
     .then((data) => {
+
+      // create a new instance of the manager class extending from employee class
       const manager = new Manager(
         data.name,
         data.id,
         data.email,
         data.officeNumber
       );
+
+      // push details into array
       team.push(manager);
+
+      // call menu function to display the options to gather more employee details
       menuOptions();
     });
 }
 
+
+// Function to display options for creating more profiles
 function menuOptions() {
   return inquirer
     .prompt([
@@ -69,6 +78,8 @@ function menuOptions() {
       },
     ])
     .then((data) => {
+
+      // check which list item has been selected and display the corresponding function
       switch (data.menu) {
         case "Add an engineer":
           engineerInfo();
@@ -78,13 +89,15 @@ function menuOptions() {
           break;
         case "Finish building the team":
           console.log("Your team's profile is now ready to view")
+
+          // write the html file based on information gathered
           return writeFileAsync(outputPath, render(team))
         }
       
     });
 }
 
-// Questions to prompt user for Engineer's details.
+// function to prompt user for Engineer's details.
 function engineerInfo () {
   return inquirer.prompt([
   {
@@ -109,18 +122,24 @@ function engineerInfo () {
   },
 ])
 .then(data => {
+
+  // new instance of the engineer class created
   const engineer = new Engineer(data.name,
     data.id,
     data.email,
     data.github
   );
+
+  // engineer's information pushed into team array
   team.push(engineer);
+
+  // display menu options
   menuOptions();
 })
 };
 
 
-// Questions to prompt user for Intern's details.
+// function to prompt user for Intern's details.
 function internInfo () {
   return inquirer.prompt([
   {
@@ -145,12 +164,18 @@ function internInfo () {
   },
 ])
 .then(data => {
+
+  // new instance of the employee class created with intern information
   const intern = new Intern (data.name,
     data.id,
     data.email,
     data.school
   );
+
+  // intern details pushed into team array
   team.push(intern);
+
+  // display menu options
   menuOptions();
 })
 };
